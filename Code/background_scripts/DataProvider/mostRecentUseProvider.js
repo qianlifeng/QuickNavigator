c03c1fd3770d9ceb4a8051a9692cc5c601a21fbc
qianlifeng <qianlf2008@163.com>
2013-07-05 17:21:59 +0800

    change mru source type label

--- Code/background_scripts/DataProvider/mostRecentUseProvider.js
+++ Code/background_scripts/DataProvider/mostRecentUseProvider.js
@@ -9,7 +9,7 @@ var mostRecentUseProvider = function(){
                var maxMRUCount =  options.getMRUCount();
                urls = urls.slice(0,maxMRUCount);
                urls.forEach(function(element,n,arrary){
-                   mruLists.push({title:element.title,url:element.url,sourceType:element.sourceType,relevancy:element.visitedCount}); 
+                   mruLists.push({title:element.title,url:element.url,sourceType:"MRU",relevancy:element.visitedCount}); 
                });
                return mruLists; 
         },
